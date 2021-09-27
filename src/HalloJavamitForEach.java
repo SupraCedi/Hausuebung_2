a8aea96541dc796231ea500f39027deb6b20294b
cedri <augustinc190003@sus.htl-grieskirchen.at>
2021-09-27 11:52:21 +0200

    first commit

--- src/HalloJavamitForEach.java
+++ src/HalloJavamitForEach.java
@@ -1,16 +1,25 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class HalloJavamitForEach {
-    private List<String> list = new ArrayList<>();
-
-    public HalloJavamitForEach(List<String> list) {
-        this.list = list;
-    }
-
-    public void printList() {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        
+        list.add("187");
+        list.add("test");
+        list.add("abc");
+        
         for (String numb : list) {
             System.out.println(numb);
         }
+        System.out.println("\n");
+        list.forEach((String numb) -> System.out.println(numb));
+
+        Consumer<String> consumer = (String s) -> System.out.println(s);
+
+        System.out.println("\n");
+        list.forEach(System.out::print);
+        }
     }
-}
+
