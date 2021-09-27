f129331e354d4baf66ea66fe0efa88bb447f174a
cedri <augustinc190003@sus.htl-grieskirchen.at>
2021-09-27 11:07:34 +0200

    first commit

--- src/HalloJavamitForEach.java
+++ src/HalloJavamitForEach.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class HalloJavamitForEach {
+import java.util.ArrayList;
+import java.util.List;
+
+public class HalloJavamitForEach {
+    private List<String> list = new ArrayList<>();
+
+    public HalloJavamitForEach(List<String> list) {
+        this.list = list;
+    }
+
+    public void printList() {
+        for (String numb : list) {
+            System.out.println(numb);
+        }
+    }
 }
