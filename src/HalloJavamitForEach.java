9796d8d1752fbfaf481d9620429165cd2d11b3cd
cedri <augustinc190003@sus.htl-grieskirchen.at>
2022-01-15 10:42:00 +0100

    final commit

--- src/HalloJavamitForEach.java
+++ src/HalloJavamitForEach.java
@@ -1,15 +1,20 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.function.Consumer;
 
 public class HalloJavamitForEach {
     public static void main(String[] args) {
+
+        File fileName = new File("myFile.csv");
+        String[] numbers = new String[0];
         List<String> list = new ArrayList<>();
-        
+
         list.add("187");
         list.add("test");
         list.add("abc");
-        
+
         for (String numb : list) {
             System.out.println(numb);
         }
@@ -20,6 +25,365 @@ public class HalloJavamitForEach {
 
         System.out.println("\n");
         list.forEach(System.out::print);
+        System.out.println("\n");
+
+        NumberTester nt = new NumberTester(fileName);
+        nt.testFile(fileName);
+
+        //KonsolenInterface
+        Scanner s = new Scanner(System.in);
+        final boolean run = true;
+        while (run) {
+
+            System.out.println("Enter Number a1");double a1 = s.nextInt();
+            System.out.println("Enter Number b1");double b1 = s.nextInt();
+            System.out.println("Enter Number a2");double a2 = s.nextInt();
+            System.out.println("Enter Number b2");double b2 = s.nextInt();
+            System.out.println("Choose operation:" + "\n" + "1 - add" + "\n" + "2 - subtract" + "\n" + "3 - multiply" + "\n" + "4 - divide" + "\n");
+            int operation = s.nextInt();
+            System.out.println("Choose calculator:" + "\n" + "1 - Rational calculator" + "\n" + "2 - Vector calculator" + "\n" + "3 - Complex Calculator" + "\n" + "4 - Exit program");
+            int calcu = s.nextInt();
+            switch (calcu) {
+                case 1:
+                    AbstractCalculator rationalcalculator = new RationalCalculator((a, b) -> {
+
+                        /*Double a1 = a.getA();
+                        Double b1 = b.getA();
+                        Double a2 = a.getB();
+                        Double b2 = b.getB();*/
+                        if(operation == 1) {
+                            Double result1 = a1 * b2;
+                            Double result2 = b1 * a2;
+                            Double result = result1 + result2;
+                            Double nenner = a2 * b2;
+
+
+                            for (int i = 0; i < nenner; i++) {
+                                for (int j = 0; j < nenner; j++) {
+                                    if (nenner % j == 0) {
+                                        if (result % j == 0) {
+                                            nenner = nenner / j;
+                                            result = result / j;
+                                        }
+                                    }
+                                }
+                            }
+                            System.out.println(result);
+                            System.out.println("--------------------");
+                            System.out.println(nenner);
+
+                        }
+                            return a;
+
+                    }, (a, b) -> {
+                        /*Double a1 = a.getA();
+                        Double b1 = b.getA();
+                        Double a2 = a.getB();
+                        Double b2 = b.getB();*/
+                        if(operation == 2) {
+                            Double result1 = a1 * b2;
+                            Double result2 = b1 * a2;
+                            Double result = result1 - result2;
+                            Double nenner = a2 * b2;
+
+                            for (int i = 0; i < nenner; i++) {
+                                for (int j = 0; j < nenner; j++) {
+                                    if (nenner % j == 0) {
+                                        if (result % j == 0) {
+                                            nenner = nenner / j;
+                                            result = result / j;
+                                        }
+                                    }
+                                }
+                            }
+                            System.out.println(result);
+                            System.out.println("--------------------");
+                            System.out.println(nenner);
+                        }
+                        return a;
+                    }, (a, b) -> {
+                        /*Double a1 = a.getA();
+                        Double b1 = b.getA();
+                        Double a2 = a.getB();
+                        Double b2 = b.getB();*/
+                        if(operation == 3) {
+                            Double result1 = a1 * b2;
+                            Double result2 = b1 * a2;
+                            Double result = result1 * result2;
+                            Double nenner = a2 * b2;
+
+                            for (int i = 0; i < nenner; i++) {
+                                for (int j = 0; j < nenner; j++) {
+                                    if (nenner % j == 0) {
+                                        if (result % j == 0) {
+                                            nenner = nenner / j;
+                                            result = result / j;
+                                        }
+                                    }
+                                }
+                            }
+                            System.out.println(result);
+                            System.out.println("--------------------");
+                            System.out.println(nenner);
+                        }
+                        return a;
+                    }, (a, b) -> {
+                        /*Double a1 = a.getA();
+                        Double b1 = b.getA();
+                        Double a2 = a.getB();
+                        Double b2 = b.getB();*/
+                        if(operation == 4) {
+                            Double result1 = a1 * b2;
+                            Double result2 = b1 * a2;
+                            Double result = result1 / result2;
+                            Double nenner = a2 * b2;
+
+                            for (int i = 0; i < nenner; i++) {
+                                for (int j = 0; j < nenner; j++) {
+                                    if (nenner % j == 0) {
+                                        if (result % j == 0) {
+                                            nenner = nenner / j;
+                                            result = result / j;
+                                        }
+                                    }
+                                }
+                            }
+                            System.out.println(result);
+                            System.out.println("--------------------");
+                            System.out.println(nenner);
+                        }
+                        return a;
+                    });
+                    break;
+                case 2:
+                    AbstractCalculator vectorcalculator = new VectorCalculator((a, b) -> {
+                        /*Double a1 = a.getA();
+                        Double b1 = b.getA();
+                        Double a2 = a.getB();
+                        Double b2 = b.getB();*/
+                        if(operation == 1) {
+                            Double result1 = a1 * b2;
+                            Double result2 = b1 * a2;
+                            Double result = result1 + result2;
+                            Double nenner = a2 * b2;
+
+
+                            for (int i = 0; i < nenner; i++) {
+                                for (int j = 0; j < nenner; j++) {
+                                    if (nenner % j == 0) {
+                                        if (result % j == 0) {
+                                            nenner = nenner / j;
+                                            result = result / j;
+                                        }
+                                    }
+                                }
+                            }
+                            System.out.println(result);
+                            System.out.println("--------------------");
+                            System.out.println(nenner);
+                        }
+                        return a;
+
+                    }, (a, b) -> {
+                        /*Double a1 = a.getA();
+                        Double b1 = b.getA();
+                        Double a2 = a.getB();
+                        Double b2 = b.getB();*/
+                        if(operation == 2) {
+                            Double result1 = a1 * b2;
+                            Double result2 = b1 * a2;
+                            Double result = result1 - result2;
+                            Double nenner = a2 * b2;
+
+                            for (int i = 0; i < nenner; i++) {
+                                for (int j = 0; j < nenner; j++) {
+                                    if (nenner % j == 0) {
+                                        if (result % j == 0) {
+                                            nenner = nenner / j;
+                                            result = result / j;
+                                        }
+                                    }
+                                }
+                            }
+                            System.out.println(result);
+                            System.out.println("--------------------");
+                            System.out.println(nenner);
+                        }
+                        return a;
+                    }, (a, b) -> {
+                        /*Double a1 = a.getA();
+                        Double b1 = b.getA();
+                        Double a2 = a.getB();
+                        Double b2 = b.getB();*/
+                        if(operation == 3) {
+                            Double result1 = a1 * b2;
+                            Double result2 = b1 * a2;
+                            Double result = result1 * result2;
+                            Double nenner = a2 * b2;
+
+                            for (int i = 0; i < nenner; i++) {
+                                for (int j = 0; j < nenner; j++) {
+                                    if (nenner % j == 0) {
+                                        if (result % j == 0) {
+                                            nenner = nenner / j;
+                                            result = result / j;
+                                        }
+                                    }
+                                }
+                            }
+                            System.out.println(result);
+                            System.out.println("--------------------");
+                            System.out.println(nenner);
+                        }
+                        return a;
+                    }, (a, b) -> {
+                        /*Double a1 = a.getA();
+                        Double b1 = b.getA();
+                        Double a2 = a.getB();
+                        Double b2 = b.getB();*/
+                        if(operation == 4) {
+                            Double result1 = a1 * b2;
+                            Double result2 = b1 * a2;
+                            Double result = result1 / result2;
+                            Double nenner = a2 * b2;
+
+                            for (int i = 0; i < nenner; i++) {
+                                for (int j = 0; j < nenner; j++) {
+                                    if (nenner % j == 0) {
+                                        if (result % j == 0) {
+                                            nenner = nenner / j;
+                                            result = result / j;
+                                        }
+                                    }
+                                }
+                            }
+                            System.out.println(result);
+                            System.out.println("--------------------");
+                            System.out.println(nenner);
+
+                        }
+                        return a;
+                    });
+                    break;
+                case 3:
+                    AbstractCalculator complexCalculator = new ComplexCalculator((a, b) -> {
+                        /*Double a1 = a.getA();
+                        Double b1 = b.getA();
+                        Double a2 = a.getB();
+                        Double b2 = b.getB();*/
+                        if(operation == 1) {
+                            Double result1 = a1 * b2;
+                            Double result2 = b1 * a2;
+                            Double result = result1 + result2;
+                            Double nenner = a2 * b2;
+
+
+                            for (int i = 0; i < nenner; i++) {
+                                for (int j = 0; j < nenner; j++) {
+                                    if (nenner % j == 0) {
+                                        if (result % j == 0) {
+                                            nenner = nenner / j;
+                                            result = result / j;
+                                        }
+                                    }
+                                }
+                            }
+                            System.out.println(result);
+                            System.out.println("--------------------");
+                            System.out.println(nenner);
+                        }
+                        return a;
+
+                    }, (a, b) -> {
+                        /*Double a1 = a.getA();
+                        Double b1 = b.getA();
+                        Double a2 = a.getB();
+                        Double b2 = b.getB();*/
+                        if(operation == 2) {
+                            Double result1 = a1 * b2;
+                            Double result2 = b1 * a2;
+                            Double result = result1 - result2;
+                            Double nenner = a2 * b2;
+
+                            for (int i = 0; i < nenner; i++) {
+                                for (int j = 0; j < nenner; j++) {
+                                    if (nenner % j == 0) {
+                                        if (result % j == 0) {
+                                            nenner = nenner / j;
+                                            result = result / j;
+                                        }
+                                    }
+                                }
+                            }
+                            System.out.println(result);
+                            System.out.println("--------------------");
+                            System.out.println(nenner);
+                        }
+                        return a;
+                    }, (a, b) -> {
+                        /*Double a1 = a.getA();
+                        Double b1 = b.getA();
+                        Double a2 = a.getB();
+                        Double b2 = b.getB();*/
+                        if(operation == 3) {
+                            Double result1 = a1 * b2;
+                            Double result2 = b1 * a2;
+                            Double result = result1 * result2;
+                            Double nenner = a2 * b2;
+
+                            for (int i = 0; i < nenner; i++) {
+                                for (int j = 0; j < nenner; j++) {
+                                    if (nenner % j == 0) {
+                                        if (result % j == 0) {
+                                            nenner = nenner / j;
+                                            result = result / j;
+                                        }
+                                    }
+                                }
+                            }
+                            System.out.println(result);
+                            System.out.println("--------------------");
+                            System.out.println(nenner);
+                        }
+                        return a;
+                    }, (a, b) -> {
+                        /*Double a1 = a.getA();
+                        Double b1 = b.getA();
+                        Double a2 = a.getB();
+                        Double b2 = b.getB();*/
+                        if(operation == 4) {
+                            Double result1 = a1 * b2;
+                            Double result2 = b1 * a2;
+                            Double result = result1 / result2;
+                            Double nenner = a2 * b2;
+
+                            for (int i = 0; i < nenner; i++) {
+                                for (int j = 0; j < nenner; j++) {
+                                    if (nenner % j == 0) {
+                                        if (result % j == 0) {
+                                            nenner = nenner / j;
+                                            result = result / j;
+                                        }
+                                    }
+                                }
+                            }
+                            System.out.println(result);
+                            System.out.println("--------------------");
+                            System.out.println(nenner);
+                        }
+                        return a;
+                    });
+                    break;
+
+                case 4:
+                    System.out.println("Program beendet");
+                    System.exit(0);
+                    break;
+
+            }
+
         }
     }
+}
+
 
--- src/NumberTester.java
+++ src/NumberTester.java
@@ -1,21 +1,108 @@
-public class NumberTester{
-    public NumberTester(String fileName){
+import java.io.*;
 
+public class NumberTester {
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+    private NumberTest palindromeTester;
+    File fileName;
+    String[] numbers;
+
+    public NumberTester(File fileName) {
+        this.fileName = fileName;
     }
 
-    public void setOddEvenTester(NumberTest oddTester){
+
+    public void setOddEvenTester(NumberTest oddTester) {
         this.oddTester = oddTester;
     }
 
-    public void setPrimeTester(NumberTest primeTester){
+    public void setPrimeTester(NumberTest primeTester) {
         this.primeTester = primeTester;
+
     }
 
-    public void setPalindromeTester(NumberTest palindromeTester){
+    public void setPalindromeTester(NumberTest palindromeTester) {
         this.palindromeTester = palindromeTester;
     }
 
-    public void testFile(){
+    public void testFile(File fileName) {
+        try (BufferedReader br = new BufferedReader(new FileReader("myFile.txt"));) {
+            String zeile = br.readLine();
+            while ((zeile = br.readLine()) != null) {
+                String[] parts = zeile.split(" ");
+                int number = Integer.parseInt(parts[1]);
 
+                switch (parts[0]) {
+                    case "1":
+                        setOddEvenTester((int n) -> n % 2 == 0);
+                        if (oddTester.testNumber(number)) {
+                            System.out.println("EVEN");
+                        } else {
+                            System.out.println("ODD");
+                        }
+
+                        break;
+
+                    case "2":
+                        setPrimeTester((int n) -> prime(n));
+                        if (primeTester.testNumber(number)) {
+                            System.out.println("PRIME");
+                        } else {
+                            System.out.println("NO PRIME");
+                        }
+
+                        break;
+
+                    case "3":
+                        setPalindromeTester((int n)-> palindrom(n));
+                        if (palindromeTester.testNumber(number)) {
+                            System.out.println("PALINDROM");
+                        } else {
+                            System.out.println("NO PALINDROM");
+                        }
+
+                        break;
+
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public boolean testNumber(int number) {
+        return false;
+    }
+
+    public boolean prime(int number) {
+        int temp;
+        boolean isPrime = true;
+
+        for (int i = 2; i<= number/2;i++){
+            temp=number%i;
+            if (temp==0){
+                isPrime=false;
+                break;
+            }
+        }
+        return isPrime;
+    }
+
+    public boolean palindrom(int number) {
+        int palindrome = number;
+        int reverse = 0;
+
+        while (palindrome != 0) {
+            int reminder = palindrome % 10;
+            reverse = reverse * 10 - reminder;
+            palindrome = palindrome / 10;
+        }
+        if (number == reverse) {
+            return true;
+        }
+            return false;
     }
 }
